628fc98301728ad124e7a851a94d1eb81db2f7a3
AndresArdila89 <andresardila@me.com>
2020-09-16 16:25:58 -0400

    add class db

---
+++ Lab1_ConnectedMode2/DAL/UtilityDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;  //required for ADO.net object model
+
+
+namespace Lab1_ConnectedMode2.DAL
+{
+    public static class UtilityDB
+    {
+
+        public static SqlConnection ConnectDB()
+        {
+            SqlConnection connDB = new SqlConnection();
+            connDB.ConnectionString = "server=192.168.50.138;database=EmployeeDB;user=sa;password=Ing753951;";
+            connDB.Open();
+
+
+            return connDB;
+        }
+    }
+}
