e63cf902d8d67a75f6c4a5fe974e9886bfc51860
AndresArdila89 <andresardila@me.com>
2020-09-16 21:52:01 -0400

    fix database connection

---
+++ Lab1_ConnectedMode2/DAL/PDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_ConnectedMode2.DAL
+{
+    static class PDB
+    {
+       public const string serverName = "192.168.50.138";
+       public const string db = "EmployeeDB";
+       public const string user = "sa";
+       public const string pwd = "montreal2020";
+    }
+}
--- Lab1_ConnectedMode2/GUI/FormTest.Designer.cs
+++ Lab1_ConnectedMode2/GUI/FormTest.Designer.cs
@@ -33,13 +33,15 @@
             this.validate = new System.Windows.Forms.Button();
             this.create = new System.Windows.Forms.Button();
             this.display = new System.Windows.Forms.Button();
+            this.openDB = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // writeData
             // 
-            this.writeData.Location = new System.Drawing.Point(520, 150);
+            this.writeData.Location = new System.Drawing.Point(347, 97);
+            this.writeData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.writeData.Name = "writeData";
-            this.writeData.Size = new System.Drawing.Size(226, 69);
+            this.writeData.Size = new System.Drawing.Size(151, 45);
             this.writeData.TabIndex = 0;
             this.writeData.Text = "Write Data";
             this.writeData.UseVisualStyleBackColor = true;
@@ -47,9 +49,10 @@
             // 
             // readData
             // 
-            this.readData.Location = new System.Drawing.Point(520, 235);
+            this.readData.Location = new System.Drawing.Point(347, 153);
+            this.readData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.readData.Name = "readData";
-            this.readData.Size = new System.Drawing.Size(226, 69);
+            this.readData.Size = new System.Drawing.Size(151, 45);
             this.readData.TabIndex = 1;
             this.readData.Text = "Read Data";
             this.readData.UseVisualStyleBackColor = true;
@@ -57,18 +60,20 @@
             // 
             // validate
             // 
-            this.validate.Location = new System.Drawing.Point(520, 320);
+            this.validate.Location = new System.Drawing.Point(347, 208);
+            this.validate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.validate.Name = "validate";
-            this.validate.Size = new System.Drawing.Size(226, 69);
+            this.validate.Size = new System.Drawing.Size(151, 45);
             this.validate.TabIndex = 2;
             this.validate.Text = "Validate";
             this.validate.UseVisualStyleBackColor = true;
             // 
             // create
             // 
-            this.create.Location = new System.Drawing.Point(520, 408);
+            this.create.Location = new System.Drawing.Point(347, 265);
+            this.create.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.create.Name = "create";
-            this.create.Size = new System.Drawing.Size(226, 69);
+            this.create.Size = new System.Drawing.Size(151, 45);
             this.create.TabIndex = 3;
             this.create.Text = "Create Employee List";
             this.create.UseVisualStyleBackColor = true;
@@ -76,24 +81,37 @@
             // 
             // display
             // 
-            this.display.Location = new System.Drawing.Point(520, 496);
+            this.display.Location = new System.Drawing.Point(347, 322);
+            this.display.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.display.Name = "display";
-            this.display.Size = new System.Drawing.Size(226, 69);
+            this.display.Size = new System.Drawing.Size(151, 45);
             this.display.TabIndex = 4;
             this.display.Text = "Display Employee List";
             this.display.UseVisualStyleBackColor = true;
             this.display.Click += new System.EventHandler(this.display_Click);
             // 
+            // openDB
+            // 
+            this.openDB.Location = new System.Drawing.Point(347, 380);
+            this.openDB.Margin = new System.Windows.Forms.Padding(2);
+            this.openDB.Name = "openDB";
+            this.openDB.Size = new System.Drawing.Size(151, 45);
+            this.openDB.TabIndex = 5;
+            this.openDB.Text = "Open DB";
+            this.openDB.UseVisualStyleBackColor = true;
+            // 
             // FormTest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 695);
+            this.ClientSize = new System.Drawing.Size(533, 566);
+            this.Controls.Add(this.openDB);
             this.Controls.Add(this.display);
             this.Controls.Add(this.create);
             this.Controls.Add(this.validate);
             this.Controls.Add(this.readData);
             this.Controls.Add(this.writeData);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormTest";
             this.Text = "FormTest";
             this.ResumeLayout(false);
@@ -107,5 +125,6 @@
         private System.Windows.Forms.Button validate;
         private System.Windows.Forms.Button create;
         private System.Windows.Forms.Button display;
+        private System.Windows.Forms.Button openDB;
     }
 }
